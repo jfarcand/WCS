356c6d49bae876cf281bf0b8e89d2a82e3e386a1
Sebastian G. Vinci <svinci@despegar.com>
2015-08-12 11:10:41 -0300

    scala 2.11 support

--- src/test/scala/org/jfarcand/wcs/test/BaseTest.scala
+++ src/test/scala/org/jfarcand/wcs/test/BaseTest.scala
@@ -9,40 +9,32 @@ import javax.servlet.http.{ HttpServletResponse, HttpServletRequest }
 import org.eclipse.jetty.server.{ Request, Server }
 
 import org.scalatest.{ FlatSpec, BeforeAndAfterAll }
-import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.Matchers
 
-abstract class BaseTest extends Server with FlatSpec with BeforeAndAfterAll with ShouldMatchers {
+trait BaseTest extends FlatSpec with BeforeAndAfterAll with Matchers {
+
+  protected val server: Server = new Server()
   protected final val log: Logger = LoggerFactory.getLogger(classOf[BaseTest])
   protected var port1: Int = 0
   private var _connector: SelectChannelConnector = null
 
-  override def beforeAll(configMap: Map[String, Any]) = {
-    setUpGlobal
-  }
-
-  override def afterAll(configMap: Map[String, Any]) = {
-    tearDownGlobal
-  }
-
-  def setUpGlobal = {
+  override protected def beforeAll(): Unit =  {
     port1 = findFreePort
     _connector = new SelectChannelConnector
     _connector.setPort(port1)
-    addConnector(_connector)
+    server.addConnector(_connector)
     val _wsHandler: BaseTest#WebSocketHandler = getWebSocketHandler
-    setHandler(_wsHandler)
-    start
+    server.setHandler(_wsHandler)
+    server.start()
     log.info("Local HTTP server started successfully")
   }
 
-  def tearDownGlobal = {
-    stop
-  }
+  override protected def afterAll(): Unit = server.stop()
 
   abstract class WebSocketHandler extends HandlerWrapper with WebSocketFactory.Acceptor {
 
     def getWebSocketFactory: WebSocketFactory = {
-      return _webSocketFactory
+      _webSocketFactory
     }
 
     override def handle(target: String, baseRequest: Request, request: HttpServletRequest, response: HttpServletResponse): Unit = {
@@ -51,7 +43,7 @@ abstract class BaseTest extends Server with FlatSpec with BeforeAndAfterAll with
     }
 
     def checkOrigin(request: HttpServletRequest, origin: String): Boolean = {
-      return true
+      true
     }
 
     private final val _webSocketFactory: WebSocketFactory = new WebSocketFactory(this, 32 * 1024)
@@ -61,16 +53,16 @@ abstract class BaseTest extends Server with FlatSpec with BeforeAndAfterAll with
     var socket: ServerSocket = null
     try {
       socket = new ServerSocket(0)
-      return socket.getLocalPort
+      socket.getLocalPort
     } finally {
       if (socket != null) {
-        socket.close
+        socket.close()
       }
     }
   }
 
   protected def getTargetUrl: String = {
-    "ws://127.0.0.1:" + port1;
+    "ws://127.0.0.1:" + port1
   }
 
   def getWebSocketHandler: BaseTest#WebSocketHandler
---
+++ src/test/scala/org/jfarcand/wcs/test/WSSTest.scala
@@ -0,0 +1,8 @@
+package org.jfarcand.wcs.test
+
+/**
+ * Created by svinci on 12/08/15.
+ */
+class WSSTest {
+
+}
